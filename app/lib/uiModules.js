75c59019a02a488684298d3fc7b204f930aa5361
Andrey Tkachenko <falko.lab@gmail.com>
2015-10-21 11:36:49 +0300

    initial

---
+++ app/lib/uiModules.js
@@ -0,0 +1,11 @@
+// app/lib/uiModules.js
+var o = {
+	createSvgView: function(opts) {
+		var m = require("com.geraudbourdin.svgview");
+		return m.createView(opts);
+	}	
+};
+// place other modules here to use with ns or module attributes in alloy
+//_.extend(o, require(""));
+module.exports = o;
+Alloy.Globals.uiModules = o;
